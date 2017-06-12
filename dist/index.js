9e6c520fb99d3dd3ba702af8c4e2bacea02f5418
CoderProject <CoderProject@users.noreply.github.com>
2017-06-13 02:09:54 +0300

    Add initial value option
    Add ability to set an initial value so we can have choice not to animate from 0 at startup.

--- dist/index.js
+++ dist/index.js
@@ -21,6 +21,7 @@ export default class AnimateNumber extends Component {
     interval? : ?number,
     steps? : ?number,
     value : number,
+    initial : ?number,
     timing : 'linear' | 'easeOut' | 'easeIn' | () => number,
     formatter : () => {},
     onProgress : () => {},
@@ -77,7 +78,7 @@ export default class AnimateNumber extends Component {
     super(props);
     // default values of state and non-state variables
     this.state = {
-      value : 0,
+      value : this.props.initial ? this.props.initial : 0,
       displayValue : 0
     }
     this.dirty = false;
